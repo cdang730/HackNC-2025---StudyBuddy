78ada0908cea05251cf7a3448a324b2b0b36a244
Grace Zhang <glz@unc.edu>
2025-10-11 13:07:55 -0400

    update frontend1

--- app.py
+++ app.py
@@ -6,15 +6,17 @@ col1, col2 = st.columns(2)
 with col1: subject = st.selectbox("Subject: ", ["Math", "English", "History", "Science", "CS"])
 with col2: mode = st.radio("Mode: ", ["Virtual", "In-Person"])
 
-col3, col4 = st.columns(2)
+col3, col4, col5 = st.columns(3)
 with col3: time = st.selectbox("Study Time: ", ["Morning", "Afternoon", "Evening"])
 with col4: name = st.text_input("Enter your name: ")
+with col5: privacy = st.selectbox("Do you want others to find you? ", ["Yes", "No"])
 
 
 button = st.button("Find Match")
 if button:
-    new_user = {"name": name, "subject": subject, "time": time, "mode": mode}
-    save_user(new_user)
+    if privacy == "Yes":
+        new_user = {"name": name, "subject": subject, "time": time, "mode": mode}
+        save_user(new_user)
     matches = find_match(subject, mode, time, name)
     if matches:
         st.write("You have matches!")
