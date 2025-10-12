c2437546fa05bfaae9878d4be583404f0efd8e1e
Grace Zhang <glz@unc.edu>
2025-10-12 00:40:33 -0400

    changed the merge

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from backend import save_user, find_match
 from pathlib import Path
-from userlogin import register_user, login
 
 # Load and apply custom CSS from the same directory as this file.
 st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
@@ -11,14 +10,6 @@ if not st.user.is_logged_in:
         st.login()
     st.stop()
 
-if st.button("Log out"):
-    st.logout()
-st.markdown(f"Welcome! {st.user.name}")
-
-st.title("Studdy Buddy Planner")
-col1, col2 = st.columns(2)
-with col1: subject = st.selectbox("Subject: ", ["Math", "English", "History", "Science", "CS"])
-with col2: mode = st.radio("Mode: ", ["Virtual", "In-Person"])
 
 if st.button("Log out"):
     st.logout()
@@ -28,13 +19,6 @@ st.title("Studdy Buddy Planner")
 col1, col2 = st.columns(2)
 with col1: subject = st.selectbox("Subject: ", ["Math", "English", "History", "Science", "CS"])
 with col2: mode = st.radio("Mode: ", ["Virtual", "In-Person"])
-
-col3, col4, col5 = st.columns(3)
-with col3: time = st.selectbox("Study Time: ", ["Morning", "Afternoon", "Evening"])
-with col4: name = st.text_input("Enter your name: ")
-with col5: privacy = st.selectbox("Do you want others to find you? ", ["Yes", "No"])
-
-contact = st.text_input("Contact information: ")
 
 col3, col4, col5 = st.columns(3)
 with col3: time = st.selectbox("Study Time: ", ["Morning", "Afternoon", "Evening"])
@@ -56,17 +40,3 @@ if button:
             st.write(m["name"], "wants to study", m["subject"], "in the ", m["time"].lower(), "(", m["mode"], "), Contact: ", m["contact"])
     elif not matches:
         st.write("Sorry, no matches yet. Come back later!")
-button = st.button("Find Match")
-if button:
-    if privacy == "Yes":
-        new_user = {"name": name, "subject": subject, "time": time, "mode": mode, "contact": contact}
-        save_user(new_user)
-    matches = find_match(subject, mode, time, name, contact)
-    if matches:
-        st.write("You have matches!")
-        for m in matches:
-            st.write(m["name"], "wants to study", m["subject"], "in the ", m["time"].lower(), "(", m["mode"], "), Contact: ", m["contact"])
-    elif not matches:
-        st.write("Sorry, no matches yet. Come back later!")
-
-
